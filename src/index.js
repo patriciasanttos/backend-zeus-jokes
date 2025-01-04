f1b7e9691bf363ea62069988447822c8e44f209f
Patricia Santos <patriciasanttos.dev@gmail.com>
2025-01-04 12:08:59 -0300

    versão mockada

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from "express"
+import cors from "cors"
+import {piadas} from "./mock.js"
+
+const app = express()
+app.use(
+  cors(
+    {
+      exposeHeaders: ["X-Total-Count"]
+    }
+  )
+)
+app.use(express.json())
+
+function getRandomIt(max) {
+  return Math.floor(Math.random() * max)
+}
+
+app.get("/batata", (request, response) => {
+  return response.status(200).send({batata: true})
+})
+
+app.get("/piadas", (request, response) => {
+  const randomNum = getRandomIt(piadas.length)
+  return response.status(200).send({piada: piadas[randomNum]})
+})
+
+app.listen("3000")
---
+++ src/mock.js
@@ -0,0 +1,248 @@
+export const piadas = [
+  {
+    piada: "Qual é o peixe mais inteligente? O atum!",
+    tipo: "biologia",
+    answer: null,
+  },
+  {
+    piada:
+      "Por que o livro de matemática ficou triste? Porque tinha muitos problemas.",
+    tipo: "escola",
+    answer: null,
+  },
+  {
+    piada:
+      "Qual é o cúmulo do eletricista? Ficar de mau-humor e não dar fase pra ninguém.",
+    tipo: "profissões",
+    answer: null,
+  },
+  {
+    piada:
+      "Sabe por que o lápis nunca se atrasava? Porque ele sempre estava na ponta!",
+    tipo: "escola",
+    answer: null,
+  },
+  {
+    piada: "O que é, o que é: Tem bico mas não canta?",
+    tipo: "adivinhação",
+    answer: "O bule.",
+  },
+  {
+    piada: "O que é, o que é: Anda com os pés na cabeça?",
+    tipo: "adivinhação",
+    answer: "O piolho.",
+  },
+  {
+    piada: "O que é, o que é: Tem dentes mas não morde?",
+    tipo: "adivinhação",
+    answer: "O pente.",
+  },
+  {
+    piada: "O que é, o que é: Sempre se quebra quando se fala?",
+    tipo: "adivinhação",
+    answer: "O segredo.",
+  },
+  {
+    piada:
+      "O que é, o que é: Não tem olhos, mas pisca. Não tem boca, mas fala?",
+    tipo: "adivinhação",
+    answer: "O farol.",
+  },
+  {
+    piada: "O que é, o que é: Quanto mais quente, mais fresco?",
+    tipo: "adivinhação",
+    answer: "O pão.",
+  },
+  {
+    piada: "O que é, o que é: Cai em pé e corre deitado?",
+    tipo: "adivinhação",
+    answer: "A chuva.",
+  },
+  {
+    piada:
+      "O que é, o que é: Não é vivo, mas cresce. Não tem pulmões, mas precisa de ar?",
+    tipo: "adivinhação",
+    answer: "O fogo.",
+  },
+  {
+    piada: "O que é, o que é: Fica cheio durante o dia e vazio à noite?",
+    tipo: "adivinhação",
+    answer: "O sapato.",
+  },
+  {
+    piada: "O que é, o que é: Tem asa, mas não voa. Tem cabeça, mas não pensa?",
+    tipo: "adivinhação",
+    answer: "A cadeira.",
+  },
+  {
+    piada: "O que é, o que é: Não se come, mas é bom para comer?",
+    tipo: "adivinhação",
+    answer: "O garfo.",
+  },
+  {
+    piada: "O que é, o que é: Quanto mais se tira, mais se aumenta?",
+    tipo: "adivinhação",
+    answer: "A dívida.",
+  },
+  {
+    piada: "O que é, o que é: Sobe quando a chuva desce?",
+    tipo: "adivinhação",
+    answer: "O guarda-chuva.",
+  },
+  {
+    piada: "O que é, o que é: Tem uma perna mais curta e dança o tempo todo?",
+    tipo: "adivinhação",
+    answer: "O compasso.",
+  },
+  {
+    piada: "O que é, o que é: Corre, mas não tem pernas?",
+    tipo: "adivinhação",
+    answer: "O rio.",
+  },
+  {
+    piada: "O que é, o que é: Fica no meio do ovo?",
+    tipo: "adivinhação",
+    answer: "A letra V.",
+  },
+  {
+    piada: "O que é, o que é: É surdo, mas escuta tudo?",
+    tipo: "adivinhação",
+    answer: "O ouvido.",
+  },
+  {
+    piada: "O que é, o que é: Tem formato de círculo, mas não é redondo?",
+    tipo: "adivinhação",
+    answer: "O anel.",
+  },
+  {
+    piada: "O que é, o que é: Se corta, mas não sangra?",
+    tipo: "adivinhação",
+    answer: "O papel.",
+  },
+  {
+    piada: "O que é, o que é: Não é ave, mas tem asas e voa?",
+    tipo: "adivinhação",
+    answer: "O avião.",
+  },
+  {
+    piada: "Qual é a bebida favorita do técnico em informática?",
+    tipo: "trocadilho",
+    answer: "O chá-mado 'boot'.",
+  },
+  {
+    piada: "O que o pão disse para o queijo?",
+    tipo: "trocadilho",
+    answer: "Tô 'quente', vem derreter comigo.",
+  },
+  {
+    piada: "Por que o computador foi ao médico?",
+    tipo: "trocadilho",
+    answer: "Porque estava com vírus.",
+  },
+  {
+    piada: "Qual é o animal mais rápido da floresta?",
+    tipo: "biologia",
+    answer: "A moto-serra.",
+  },
+  {
+    piada: "Qual é o único lugar onde o sucesso vem antes do trabalho?",
+    tipo: "escola",
+    answer: "No dicionário.",
+  },
+  {
+    piada:
+      "O que é, o que é: Vermelho por fora, branco por dentro e dá risada?",
+    tipo: "adivinhação",
+    answer: "O palhaço.",
+  },
+  {
+    piada: "Por que o cachorro entrou na igreja?",
+    tipo: "trocadilho",
+    answer: "Porque ele era um cão fiel.",
+  },
+  {
+    piada: "O que o tomate disse para o outro no semáforo?",
+    tipo: "trocadilho",
+    answer: "Deixa eu passar que tô vermelho.",
+  },
+  {
+    piada: "Por que o boi foi ao cinema?",
+    tipo: "fazenda",
+    answer: "Porque queria ver o 'trailer' do pasto.",
+  },
+  {
+    piada: "O que é, o que é: Tem 4 patas, mas não anda?",
+    tipo: "adivinhação",
+    answer: "A mesa.",
+  },
+  {
+    piada: "O que o zero disse para o oito?",
+    tipo: "trocadilho",
+    answer: "Que cinto apertado!",
+  },
+  {
+    piada: "O que o eletricista disse ao terminar o trabalho?",
+    tipo: "profissões",
+    answer: "Terminei e fiz um curto trabalho.",
+  },
+  {
+    piada: "Qual é a fruta mais explosiva?",
+    tipo: "comida",
+    answer: "A granada.",
+  },
+  {
+    piada: "O que é, o que é: Pode ser feito de vidro, mas não quebra?",
+    tipo: "adivinhação",
+    answer: "O reflexo.",
+  },
+  {
+    piada: "O que é, o que é: Está sempre na frente, mas nunca se move?",
+    tipo: "adivinhação",
+    answer: "O farol do carro.",
+  },
+  {
+    piada: "O que é, o que é: Quanto mais limpa, mais suja fica?",
+    tipo: "adivinhação",
+    answer: "A água do banho.",
+  },
+  {
+    piada: "O que o jacaré falou pro detetive?",
+    tipo: "trocadilho",
+    answer: "Já carece de provas?",
+  },
+  {
+    piada: "O que o pato falou para a pata?",
+    tipo: "trocadilho",
+    answer: "Vem Quá!",
+  },
+  {
+    piada: "Por que o elefante não usa creme dental?",
+    tipo: "biologia",
+    answer: "Porque ele já tem pasta de dente na tromba!",
+  },
+  {
+    piada: "O que é, o que é: Tem pescoço mas não tem cabeça?",
+    tipo: "adivinhação",
+    answer: "A garrafa.",
+  },
+  {
+    piada: "O que é, o que é: Tem cara mas não fala?",
+    tipo: "adivinhação",
+    answer: "A moeda.",
+  },
+  {
+    piada: "Por que o livro é sempre a melhor companhia?",
+    tipo: "escola",
+    answer: "Porque ele tem muitas histórias para contar.",
+  },
+  {
+    piada: "O que é, o que é: É cheio de buracos, mas guarda água?",
+    tipo: "adivinhação",
+    answer: "A esponja.",
+  },
+  {
+    piada: "Por que o lápis não quer mais escrever?",
+    tipo: "trocadilho",
+    answer: "Porque ele está sem ponta.",
+  },
+];
